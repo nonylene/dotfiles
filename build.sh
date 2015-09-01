32fa17f36fb5dda1db02582e12083bf72d6b1ce1
nonylene <cookie.speaker@gmail.com>
2015-09-01 04:01:52 +0000

    neobundle

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ for f in .*;do
         ln -s ~/dotfiles/$f ~/$f
     fi
 done
+
+if [ ! -e ~/.vim/bundle ];then
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    echo "Please :NeoBundleInstall on vim!"
+fi
