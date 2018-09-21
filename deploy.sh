1363f81d0779e7067feab9a547d2e1c4a7ff342a
nonylene <cookie.speaker@gmail.com>
2018-09-21 16:12:24 +0900

    tmux: local conf

--- deploy.sh
+++ deploy.sh
@@ -68,13 +68,13 @@ function cp_local_unless_exists() {
 
 cp_local_unless_exists '.zshrc_local'
 cp_local_unless_exists '.gitconfig_local'
+cp_local_unless_exists '.tmux.conf_local'
 
-if [ -f ~/.gituser ];then
-  echo -e "\033[1;31mPlease remove ~/.gituser !!! no longer readed\033[0m"
+if [ -f ~/.tmux-server.conf ];then
+  echo -e "\033[1;31mPlease remove ~/.tmux-server.conf !!! no longer readed\033[0m"
 fi
 
 touch_unless_exists '.bashrc_local'
-touch_unless_exists '.tmux-server.conf'
 
 # vim
 plug_file="$HOME/.vim/autoload/plug.vim"
